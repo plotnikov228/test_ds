0b89b01cb6bdd13a307b48dd583b5ad6795e827b
plotnikov228 <plotnikoffroman@gmail.com>
2022-10-06 02:23:59 +0300

    final

--- lib/screens/web_view_screen.dart
+++ lib/screens/web_view_screen.dart
@@ -11,26 +11,30 @@ class WebViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Completer<WebViewController> _controller = Completer<WebViewController>();
-    return Scaffold(
-        body: WebView(
-        initialUrl: url,
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
-        },
-        onProgress: (int progress) {
-          print('WebView is loading (progress : $progress%)');
-        },
-        onPageStarted: (String url) {
-          print('Page started loading: $url');
-        },
-        onPageFinished: (String url) {
-          print('Page finished loading: $url');
-        },
-        gestureNavigationEnabled: true,
-        backgroundColor: const Color(0x00000000),
-    ),
-      );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Мелбет",
+      home: Scaffold(
+          body: WebView(
+          initialUrl: url,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          onProgress: (int progress) {
+            print('WebView is loading (progress : $progress%)');
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) {
+            print('Page finished loading: $url');
+          },
+          gestureNavigationEnabled: true,
+          backgroundColor: const Color(0x00000000),
+      ),
+        ),
+    );
   }
 }
 
